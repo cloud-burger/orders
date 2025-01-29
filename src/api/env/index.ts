8bfa2ecda8c104bca7e3da1878e85e0c794114d8
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2025-01-28 21:05:59 -0300

    chore: remove old vars

--- src/api/env/index.ts
+++ src/api/env/index.ts
@@ -9,10 +9,6 @@ interface EnvSchemaProps {
   DATABASE_PORT: string;
   DATABASE_HOST: string;
   DATABASE_CONNECTION_TIMEOUT: string;
-  NOTIFICATION_WEBHOOK_URL: string;
-  MERCADO_PAGO_CREATE_QR_API_URL: string;
-  MERCADO_PAGO_GET_QR_INFO_API_URL: string;
-  MERCADO_PAGO_API_TOKEN: string;
 }
 
 export const envSchema = Joi.object({
@@ -23,10 +19,6 @@ export const envSchema = Joi.object({
   DATABASE_PORT: Joi.string().required(),
   DATABASE_HOST: Joi.string().required(),
   DATABASE_CONNECTION_TIMEOUT: Joi.string().required(),
-  NOTIFICATION_WEBHOOK_URL: Joi.string().required(),
-  MERCADO_PAGO_CREATE_QR_API_URL: Joi.string().required(),
-  MERCADO_PAGO_GET_QR_INFO_API_URL: Joi.string().required(),
-  MERCADO_PAGO_API_TOKEN: Joi.string().required(),
 });
 
 const { value } = envSchema.validate(process.env);
